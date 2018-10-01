d69ab8099478c02bbac89d683cd3e1831d45bc5d
bbarchu <bbarchu@gmail.com>
2018-09-30 21:43:39 -0300

    agrego al registro que solo diga la hora y que este en una sola linea

--- src/registro.cpp
+++ src/registro.cpp
@@ -6,7 +6,8 @@
 
 using namespace std; // se usa esta linea para no anteponer en cada cout el std
 
-
+#include <time.h>
+struct tm* gmtime(const time_t* tPtr);
 
 void LogEventos::registrar(LogEventos::TipoLog tipoEvento,const char* evento_ocurrido){
 	if(tipoLog >= tipoEvento){
@@ -17,12 +18,20 @@ void LogEventos::registrar(LogEventos::TipoLog tipoEvento,const char* evento_ocu
 				archivo.open("registro.txt",ios::app); //app de append
 
 			}
-		char *hora_actual;
+		/*char *hora_actual;
     	time_t tAct = time(NULL);
-    	hora_actual=asctime(localtime(&tAct));	
+    	hora_actual=asctime(localtime(&tAct));
 
-    	archivo << "Fecha&Hora: " << hora_actual;
-		archivo << "Nombre evento: " <<  evento_ocurrido << endl;
+		cout << "dd-mm-aaaa: " << pt1->tm_mday << "-" << pt1->tm_mon+1 << "-"
+                          << pt1->tm_year+1900 << endl;
+
+		*/
+
+    	time_t tSac = time(NULL);
+    	struct tm* pt1 = localtime(&tSac);
+
+    	archivo << "Hora actual: " << pt1->tm_hour << ":" << pt1->tm_min << ":" << pt1->tm_sec << "   Nombre evento: " << evento_ocurrido << endl;
+    
 
 		archivo.close();
 
