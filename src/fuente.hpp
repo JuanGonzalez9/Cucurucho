6833c87ee3b1aae63df3f3077cdc0674e30b8471
sjdalessandro <sjdalessandro@yahoo.com.ar>
2018-11-27 16:34:22 -0300

    Ahora el cliente muestra el puntaje

--- src/fuente.hpp
+++ src/fuente.hpp
@@ -1,6 +1,8 @@
 #ifndef FUENTE_HPP
 #define FUENTE_HPP
 
+#include <string>
+
 extern "C"
 {
 	#include <SDL2/SDL.h>
@@ -14,8 +16,12 @@ public:
 	~fuente ();
 	void dibujar (SDL_Renderer *renderer, const char *texto, int x, int y, const SDL_Color &color, int &w, int &h);
 	void dibujar (SDL_Renderer *renderer, const char *texto, int x, int y, const SDL_Color &color);
+	void borde (int ancho, SDL_Color color);
 protected:
-	TTF_Font *f;
+	TTF_Font *f, *fo;
+	std::string camino;
+	int puntos, ancho_borde;
+	SDL_Color color_borde;
 };
 
 #endif
