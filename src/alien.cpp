c7b750f25aca71e2540e1e0bb955178b854dd4b0
sjdalessandro <sjdalessandro@yahoo.com.ar>
2018-11-28 14:11:27 -0300

    El alien ahora salta

--- src/alien.cpp
+++ src/alien.cpp
@@ -5,8 +5,8 @@
 #include <cmath>
 
 static const int ancho = 148;
-static const float v0 = -20.0;
-static const float aceleracion = 0.9;
+static const float v0 = -14.0;
+static const float aceleracion = 1.2;
 
 alien::alien(juego* j, int x,int y, int vidas, int coordenada, int nivel):
 	Enemigo(x, y, vidas, coordenada, nivel),
@@ -35,15 +35,33 @@ void alien::cambiar_estado(estado e)
 	marco = 0;
 	switch (e) {
 		case reptando:
+			rectOrigen.x = 1;
+			rectOrigen.y = 0;
+			rectOrigen.h = 80;
+			rectDestino.h = rectOrigen.h;
 			break;
 		case caminando:
+			rectOrigen.x = 1;
+			rectOrigen.y = 0;
+			rectOrigen.h = 80;
+			rectDestino.h = rectOrigen.h;
 			break;
 		case saltando:
 			velocidad = v0;
 			tmp = rectDestino.y;
+			marco = 4;
+			rectOrigen.x = 1360;
+			rectOrigen.y = marco*140;
+			rectOrigen.h = 140;
+			rectDestino.h = rectOrigen.h;
+			rectDestino.y += 20;
 			altura = rectDestino.y;
 			break;
 		case cayendo:
+			rectOrigen.x = 1;
+			rectOrigen.y = 0;
+			rectOrigen.h = 80;
+			rectDestino.h = rectOrigen.h;
 			break;
 	};
 }
@@ -94,7 +112,7 @@ void alien::actualizar_caminando()
 		}
 	}
 	contador++;
-	if (sentido > 0 && rectDestino.x >= 600) {
+	if (sentido > 0 && rectDestino.x >= 400) {
 		cambiar_estado(saltando);
 	} else if (sentido < 0 && rectDestino.x <= 200) {
 		cambiar_estado(saltando);
@@ -138,11 +156,36 @@ void alien::actualizar_reptando()
 
 void alien::actualizar_saltando()
 {
-	altura += velocidad;
-	rectDestino.y = roundf (altura);
-	velocidad += aceleracion;
-	if (velocidad >= 0.0) {
-		cambiar_estado(cayendo);
+	if ((marco == 1 && contador % 5 == 0) || (marco != 1 && contador % 3 == 0) ) {
+		if (marco > 0) {
+			marco--;
+			rectOrigen.y = 140*marco;
+			if (marco == 3) {
+				rectDestino.y -= 30;
+				rectDestino.x += 6*sentido;
+				contador = 0;
+			} else if (marco == 2) {
+				rectDestino.y -= 26;
+				rectDestino.x += 8*sentido;
+				contador = 0;
+			} else if (marco == 1) {
+				rectDestino.y -= 25;
+				rectDestino.x -= 9*sentido;
+				contador = 0;
+			} else if (marco == 0) {
+				altura = rectDestino.y;
+				contador = 0;
+			}
+		}
+	}
+	contador++;
+	if (marco == 0) {
+		altura += velocidad;
+		rectDestino.y = roundf (altura);
+		velocidad += aceleracion;
+		if (velocidad >= 0.0) {
+			cambiar_estado(cayendo);
+		}
 	}
 }
 
